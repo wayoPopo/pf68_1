fcac78246736095b3d2e76ecf8452ff99dcbe1f4
wayoPopo <150894844+wayoPopo@users.noreply.github.com>
2025-09-04 00:26:20 +0700

    Create function_s_1234_W9.py
    add file template 1-7

---
+++ function_s_1234_W9.py
@@ -0,0 +1,14 @@
+def greet_user(name):
+  pass
+def calculate_rectangle_area(width, height):
+  pass
+def def is_palindrome(word):
+  pass
+def find_max(numbers):
+  pass
+def power(base, exponent=2):
+  pass
+def factorial(n):
+  pass
+def print_base6(n):
+  pass
